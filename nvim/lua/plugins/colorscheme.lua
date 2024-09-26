726ae855c3bb356588b8133d397d39994f097bc9
yajiuma <masaru.nakabayashi@gmail.com>
2024-09-26 14:53:49 +0900

    fix colorscheme config syntax

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,17 +2,39 @@
 -- https://github.com/topics/neovim-colorscheme
 
 return {
-  "craftzdog/solarized-osaka.nvim",
-  "rebelot/kanagawa.nvim",
-  "kepano/flexoki-neovim",
-  "tanvirtin/monokai.nvim",
-  "olimorris/onedarkpro.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = false,
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = false,
+        undercurl = true,
+      }
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
       undercurl = true,
-    }
-  end,
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false, -- do not set background color
+      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+    },
+  },
+  {
+    "kepano/flexoki-neovim",
+  },
+  {
+    "tanvirtin/monokai.nvim",
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+  },
 }
