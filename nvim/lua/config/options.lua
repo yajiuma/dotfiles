f9b3b98d919db44728a7ebb8dfc200d299ac3d6f
yajiuma <masaru.nakabayashi@gmail.com>
2024-09-26 17:04:35 +0900

    add indent settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,5 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Language settings
 vim.cmd("language en_US")
 vim.opt.helplang = "ja"
+
+-- Indent settings
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
